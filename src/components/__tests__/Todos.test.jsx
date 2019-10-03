f306ca60141ca2046de38cf6524a85c2c84d59c5
Gabriel Cabrejas <gabriel@cabrejas.net>
2019-10-03 12:16:15 +0200

    wip

--- src/components/__tests__/Todos.test.jsx
+++ src/components/__tests__/Todos.test.jsx
@@ -3,6 +3,18 @@ import React from 'react';
 import * as rtl from '@testing-library/react';
 import Todos from '../Todos';
 
+jest.mock('axios', () => {
+  return {
+    get: () => {
+      return Promise.resolve({
+        data: [
+          { name: 'sweeping', id: '1', completed: false },
+        ],
+      });
+    },
+  };
+});
+
 let tools;
 
 beforeEach(() => {
@@ -11,5 +23,8 @@ beforeEach(() => {
 });
 
 describe('Todos component', () => {
-
+  it('can render a todo that comes from api', () => {
+    const elementWithSweeping = tools.queryByText(/sweeping/);
+    expect(elementWithSweeping).toBeInTheDocument();
+  });
 });
